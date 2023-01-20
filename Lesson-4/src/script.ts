8f388c47f590cf0fa4107522e744725f6fabc1fc
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-20 15:26:26 +0600

    Lesson -4 | Dynamic Type | any type | update

--- Lesson-4/src/script.ts
+++ Lesson-4/src/script.ts
@@ -6,6 +6,8 @@ a = 10;
 
 a = "hello";
 
+// console.log(a);
+
 // any type array
 
 let b: any[] = [];
@@ -13,6 +15,8 @@ let b: any[] = [];
 b.push(10);
 b.push("hello");
 b.push(true);
+
+// console.log(b);
 
 // any type object
 
@@ -25,3 +29,5 @@ c = {
   name: "Abid",
   age: 20,
 };
+
+// console.log(c);
